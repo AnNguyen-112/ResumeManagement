9434777851dd37946348f5a834e463fd7901aea2
annguyen <annguyen01122000@gmail.com>
2024-11-16 15:18:45 -0600

    Initial Commit

---
+++ backend/Core/Entities/Job.cs
@@ -0,0 +1,20 @@
+﻿using backend.Core.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Core.Entities
+{
+    public class Job: BaseEntity
+    {
+        public string Title {  get; set; }
+        public JobLevel MyProperty { get; set; }
+
+        //Relations
+
+        
+        public long CompanyId { get; set; }
+
+        public Company Company { get; set;}
+
+        public ICollection<Candidate> Candidates { get; set; }
+    }
+}
---
+++ backend/Core/Enums/JobLevel.cs
@@ -0,0 +1,15 @@
+﻿namespace backend.Core.Enums
+{
+    public enum JobLevel
+    {
+        Intern,
+        Entry,
+        Junior,
+        MidLevel,
+        Senior,
+        Lead,
+        Cto,
+        Architect
+
+    }
+}
