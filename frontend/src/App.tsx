e1e6d259d585fe7a7ffcba586185281ddbff2f7a
annguyen <annguyen01122000@gmail.com>
2024-11-27 17:04:11 -0600

    commit 111/27/2024 5:04

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+import { useContext, lazy, Suspense } from "react";
+import { ThemeContext } from "./context/theme.context";
+import Navbar from "./Component/Navbar/Navbar.component";
+import { Routes, Route } from "react-router-dom";
+import CustomLinearProgress from "./Component/custom-linear-progress/CustomLinearProgress.component";
+
+const Home = lazy(() => import("./pages/home/Home.page"));
+
+const App = () => {
+  const { darkMode } = useContext(ThemeContext);
+
+  const appStyles = darkMode ? "app dark" : "app light";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={appStyles}>
+      <Navbar/>
+      <div className="wrapper">
+        <Suspense fallback={<CustomLinearProgress/>}>
+          <Routes>
+            <Route path="/" element={<Home></Home>}/>
+          </Routes>
+        </Suspense>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
---
+++ frontend/src/pages/home/Home.page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="content home">
+      <h3> Welcome to the website</h3>
+      <br></br>
+      <br></br>
+      <span>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto
+        inventore assumenda at mollitia blanditiis ab non distinctio labore quo
+        deleniti qui similique, voluptate numquam. Dignissimos sint vitae atque
+        aperiam nostrum consequatur dolorem quaerat, quos laudantium hic modi
+        consequuntur reiciendis magni suscipit incidunt pariatur corrupti
+        asperiores aliquam nulla! Quia pariatur, maiores impedit consequatur in
+        ab mollitia vel vitae est, harum ipsum explicabo officiis et delectus
+        amet quasi? Nobis doloribus officia assumenda a perspiciatis fuga, et
+        dolore cupiditate dignissimos iure, totam libero tempora optio
+        reprehenderit, similique nihil provident unde facere recusandae.
+        Deleniti exercitationem molestiae placeat ipsam fugiat similique. Quas
+        voluptatibus temporibus cum!
+      </span>
+    </div>
+  );
+};
+
+export default Home;
